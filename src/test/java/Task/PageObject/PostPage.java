238e2f736a2301cc6db84d6fd046d5c65d3e3289
Butoxors <butoxors@gmail.com>
2019-12-05 01:15:19 +0200

    Almost done, need finish votes

--- src/test/java/Task/PageObject/PostPage.java
+++ src/test/java/Task/PageObject/PostPage.java
@@ -2,13 +2,16 @@ package Task.PageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
 public class PostPage extends BasePage{
+    @FindBy(xpath = "//div[contains(@class, 'Post')]")
+    private WebElement _mainPost;
+
     @FindBy(xpath = "//div[@role='textbox']")
     private WebElement _commentField;
 
@@ -16,27 +19,46 @@ public class PostPage extends BasePage{
     private WebElement _sendCommentBtn;
 
     @FindAll({
-            @FindBy(xpath = "//div[@class='_3tw__eCCe7j-epNCKGXUKk ']")
+            @FindBy(xpath = "//div[contains(@class, 'Comment')]")
     })
     private List<WebElement> _actualComments;
-    private By commentXPath = By.xpath("//div[@class='_2mHuuvyV9doV3zwbZPtIPG']");
 
-    public void createComment(String commentText){
-        _commentField.click();
-        _commentField.sendKeys(commentText);
+    private By upVote = By.xpath(".//button[@aria-label='upvote' and @data-click-id='upvote']");
+    private By downVote = By.xpath(".//button[@aria-label='downvote' and @data-click-id='downvote']");
 
-        _sendCommentBtn.click();
+    public void createComment(String commentText) throws InterruptedException {
+        wait.until(ExpectedConditions.visibilityOf(_commentField));
+
+        actions.moveToElement(_commentField).click().sendKeys(commentText).build().perform();
+
+        //_sendCommentBtn.click();
     }
 
     public WebElement getCommentByUsername(String username){
-        System.out.println(_actualComments.size());
         for (WebElement comment:
-             _actualComments) {
-            WebElement e = comment.findElement(commentXPath);
-            System.out.println("Exist user: " + e.getAttribute("innerText"));
-            if (e.getAttribute("innerText") == username)
-                return e;
+                _actualComments) {
+
+            List<WebElement> commentInfo = comment.findElements(By.xpath(".//a"));
+
+            System.out.println("Exist user: " + commentInfo.get(0).getText()
+            + "; his comment time: " + commentInfo.get(1).getText());
         }
         return null;
+    }
+
+    public String upVote() {
+        WebElement upVoteElement = _mainPost.findElement(upVote);
+
+        upVoteElement.click();
+        System.out.println(upVoteElement.getAttribute("aria-pressed"));
+        return upVoteElement.getAttribute("aria-pressed");
+    }
+
+    public String downVote(){
+        WebElement downVoteElement = _mainPost.findElement(downVote);
+
+        downVoteElement.click();
+        System.out.println(downVoteElement.getAttribute("aria-pressed"));
+        return downVoteElement.getAttribute("aria-pressed");
     }
 }
